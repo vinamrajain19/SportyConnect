702bf375369f7ea01de36d553dfca217375bdca1
Vinamra Jain <91343225+vinamrajain19@users.noreply.github.com>
2023-11-19 01:52:09 +0530

    Update app.js

--- server/app.js
+++ server/app.js
@@ -23,6 +23,17 @@ app.all('*', function (req, res) {
     res.send("not found");
 })
 
+// deployment config
+const path = require("path");
+__dirname = path.resolve();
+
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "/client/build")));
+    app.get("*", (req, res) => {
+        res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+    });
+}
+
 mongoose.connect("mongodb+srv://mayank:mayank@cluster0.fy2cjib.mongodb.net/BTP?retryWrites=true&w=majority").then(() => {
     console.log('App connected to database');
     app.listen(5000, function () {
